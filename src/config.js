8a378074f33d7a9a1cd8ee1aaa6a1609e06b7946
George Mickael <george.mickael.antony@gmail.com>
2019-07-19 11:18:47 +0530

    Changes to config to deploy to heroku

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_API_URL = "http://localhost:3003/v1"
-export const BASE_SOCKET_URL = "http://localhost:3003"
+export const BASE_API_URL = "https://gmaslack-server.herokuapp.com/v1"
+export const BASE_SOCKET_URL = "https://gmaslack-server.herokuapp.com"
